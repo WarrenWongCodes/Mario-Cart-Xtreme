567a35ae42051014f7309c8b2e36db1cae392cae
Warren Wong <Warren.Wong4@gmail.com>
2020-11-19 00:08:04 -0500

    day 3 updates

--- mariocart.js
+++ mariocart.js
@@ -1,30 +1,40 @@
-let moveBy = 50;
+var moveBy = 50;
+var topPos = 0;
 
 // when pressing down arrow, move Mario 50 px up
 function upArrowPressed(){
     var element = document.getElementById("marioChar");
-    element.style.top = parseInt(element.style.top) - moveBy + 'px';
+    topPos = topPos - 50;
+    element.style.top = `${topPos}px`;
+    console.log(element.style.top);
 }
-
+    
 // when pressing down arrow, move Mario 50 px down
 function downArrowPressed(){
     var element = document.getElementById("marioChar");
-    element.style.top = parseInt(element.style.top) + moveBy + 'px';
+    topPos = topPos + 50;
+    console.log(topPos);
+    element.style.top = `${topPos}px`;
 }
-
 // Create a function that calls up and down arrow functions when keycode equals to 38 (up key), 40 (down key) 
 function moveDirection(event) {
-    switch (event.key) {
-        case "ArrowUp":
+    if(event.keyCode == 38 || event.keyCode == 40){
+        event.preventDefault();
+    }
+    switch (event.keyCode) {
+        case 38:
+            if(topPos <= 0){break;}  
             upArrowPressed();
-            break;
-        case "ArrowDown":
+        break;
+        case 40:
+            if(topPos >= 200){break;} 
             downArrowPressed();
-            break;
+        break;
         }
 };
-
-// Create a function that executes when the body loads, linking up and down key functions
-function gameStart(){
-    window.addEventListener("keydown", moveDirection);
+// when press a key moveDirection function executes
+document.addEventListener('keydown', function(event) {
+    moveDirection(event);
+    console.log('iwork');
 }
+);
